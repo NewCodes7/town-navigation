745fb1ac529d7b71538edfc95caac356225187c5
H-server <hani103201@naver.com>
2024-02-20 20:34:43 +0900

    feat: 지도 노드 및 간선 전처리 구현

--- roadNetwork/__init__.py
+++ roadNetwork/__init__.py
@@ -1,4 +1,5 @@
 import cv2
+import json
 
 # 이미지 불러오기
 image = cv2.imread('sample5.png')
@@ -13,43 +14,21 @@ blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 ret, binary = cv2.threshold(blurred, 117, 255, cv2.THRESH_BINARY)
 
 # 엣지 검출
-edges = cv2.Canny(binary, threshold1=30, threshold2=100)
+edges = cv2.Canny(binary, threshold1=10, threshold2=20)
 
 # HoughCircles 함수 호출 전에 이미지를 전처리합니다.
-circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 50, param1=10, param2=3, minRadius=3, maxRadius=7)
+circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 60, param1=10, param2=3, minRadius=2, maxRadius=15)
 
 # circles와 circles2가 None이 아닌지 확인 후에만 반복문을 실행합니다.
 if circles is not None:
     for i in circles[0]:
         cv2.circle(image, (int(i[0]), int(i[1])), int(i[2]), (255, 0, 0), 2)
 
-# 도로의 외곽선 그리기
-src = cv2.imread("sample5.png")  # 원본 이미지
-# cv_imshow(src)
-dst = src.copy()
-gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-
-ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-binary = cv2.bitwise_not(binary)
-
-contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-
-for i in range(len(contours)):
-    cv2.drawContours(dst, [contours[i]], 0, (0, 0, 0), 2)
-    # cv2.putText(dst, str(i), tuple(contours[i][0][0]), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 0), 2)
-    print(i, hierarchy[0][i])
-
-    # cv2.waitKey(0)
-
-# cv2.imshow("Contours", src)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 all_circle = []
 
 for i in range(len(circles[0])):
     tmp = [int(circles[0][i][0]), int(circles[0][i][1])]
-    print(tmp)
     all_circle.append(tmp)
 
 
@@ -85,10 +64,10 @@ def remove_non_white_edges():
 
             # 두 원의 중심 사이의 거리 계산
             distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-            print(distance)
+
             # 이미지 위에 하얀색을 포함하는지 확인
             # 인접한 원인지 확인하고, 일정 범위 내에 있다면 간선 생성
-            if distance <= 100 and has_white(x1, y1, x2, y2):
+            if distance <= 130 and has_white(x1, y1, x2, y2):
                 # 간선을 딕셔너리에 저장합니다.
                 if i2 not in edges_list:
                     edges_list[i2] = []
@@ -97,10 +76,17 @@ def remove_non_white_edges():
                 edges_list[i2].append(j)
                 edges_list[j].append(i2)
                 cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
-                print('hi')
 
 
 remove_non_white_edges()
+
+graph_data = {"nodes": all_circle,
+              "edges": edges_list}
+print(graph_data)
+
+# 노드와 간선 정보를 JSON 파일로 저장
+with open('edges_data.json', 'w') as json_file:
+    json.dump(graph_data, json_file)
 
 # 이미지 출력
 cv2.imshow("Contours", image)
@@ -116,3 +102,4 @@ cv2.destroyAllWindows()
 
 # 노드 검출 더 명확히
 # 간선 검출 더 명확히
+# 우선 길찾기 알고리즘 수행하자..!! 전처리 나중에 하자. 우선 한바퀴 도는 게 중요함.
